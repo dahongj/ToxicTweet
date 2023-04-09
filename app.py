2d415050f5d133dee1963ee53865457d1a5468eb
dahongj <dj1390@nyu.edu>
2023-04-09 20:42:29 +0000

    edit

--- app.py
+++ app.py
@@ -33,14 +33,8 @@ def siebert(data):
     label = result[0]['label']
     score = result[0]['score']
 
-    if(label == 'LABEL_0'):
-        label = 'Negative'
-    elif(label == 'LABEL_1'):
-        label = 'Neutral'
-    else:
-        label = 'Positive'
-
     return label, score 
+
 
 def getSent(data, model):
     if(model == 'Bertweet'):
