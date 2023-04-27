7c30402ff45824696feca0dddfb5051b50799ce9
dahongj <dj1390@nyu.edu>
2023-04-27 15:59:20 +0000

    space

--- app.py
+++ app.py
@@ -116,6 +116,13 @@ def rendPage():
     st.text("")
     st.text("")
     st.text("")
+    st.text("")
+    st.text("")
+    st.text("")
+    st.text("")
+    st.text("")
+    st.text("")
+    st.text("")
     #Image for sample 10 texts
     image = Image.open("milestone3.jpg")
     st.image(image, caption="10 Example Texts")
